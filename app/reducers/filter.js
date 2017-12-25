1d851a777fcf3b4b58c57b73de3c5f722bc8bfb7
Avery Choke <averychoke@Averys-MacBook-Pro.local>
2017-12-25 23:21:46 +0800

    Filter - Able to delete and add item

--- app/reducers/filter.js
+++ app/reducers/filter.js
@@ -1,4 +1,4 @@
-import { FETCH_FILTER_ITEMS, CHANGE_DATE } from '../actions/actionTypes';
+import { FETCH_FILTER_ITEMS, CHANGE_DATE, ADD_ITEM, DELETE_ITEM } from '../actions/actionTypes';
 import { combineReducers } from 'redux'
 
 const apiState = {
@@ -9,9 +9,56 @@ function dayItems(state = [], action) {
     switch (action.type) {
         case FETCH_FILTER_ITEMS:
             return !action.isFetching ? action.dayItems : state;
+        case `filter/${ADD_ITEM}`:
+            return addItem(state, action);
+        case `filter/${DELETE_ITEM}`:
+            return deleteItem(state, action);
         default:
             return state;
     }
+}
+
+// for filter/ADD_ITEM
+function addItem(dayItems, action) {
+    const { item } = action;
+    if (item && dayItems) {
+        let myDayItems = [...dayItems];
+        let dayItem = findDayItem(myDayItems, item.date);
+        if (dayItem) {
+            dayItem.addItem(item);
+        }
+        return myDayItems;
+    }
+    return dayItems;
+}
+
+// for filter/DELETE_ITEM
+function deleteItem(dayItems, action) {
+    const {ids, date} = action;
+    if (ids.length > 0) {
+        let myDayItems = [...dayItems];
+        let dayItem = findDayItem(myDayItems, date);
+        if (dayItem) {
+            dayItem.deleteItems(ids);
+            if (dayItem.items.length <= 0) {
+                const index = myDayItems.indexOf(dayItem);
+                if (index >= 0) {
+                    myDayItems.splice(index, 1);
+                }
+            }
+        }
+        return myDayItems;
+    }
+    return dayItems;
+}
+
+function findDayItem(dayItems, date) {
+    for (let i=0; i<dayItems.length; i++) {
+        if (dayItems[i].date.sameDateAs(date)) {
+            return dayItems[i];
+        }
+    }
+    return null;
 }
 
 const date = (namespace) => (state = new Date(), action) => {
